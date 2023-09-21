e858ee9eee2cb846a086932d84861eb606af863f
VitaliiKosyk <kostamtik@gmail.com>
2023-09-22 02:51:26 +0300

    BE #3 - Add Databases

--- configs/settings.py
+++ configs/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*'] if DEBUG else os.environ.get("ALLOWED_HOSTS", "")
 
 # Application definition
 
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'configs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT')
     }
 }
 
--- start.sh
+++ start.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-echo "Creating Migrations..."
+
+wait_for_postgres() {
+    until nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+        echo "PostgresSQL is not available. Waiting..."
+        sleep 1
+    done
+    echo "PostgresSQL started"
+}
+
+wait_for_postgres
+echo "PostgreSQL is ready. Proceed with your script."
+
 python manage.py makemigrations
-echo ====================================
-
-echo "Starting Migrations..."
 python manage.py migrate
-echo ====================================
-
-echo "Running Tests..."
-python manage.py test
-echo ====================================
 
 echo "Starting Server..."
 python manage.py runserver 0.0.0.0:8000
