7ed269d6ca5862924dfb6a9c3f6d522aa5f01cc2
VitaliiKosyk <kostamtik@gmail.com>
2023-09-22 21:40:19 +0300

    BE #4 - Add Migrations

--- apps/health_check/views.py
+++ apps/health_check/views.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class HealthCheck(APIView):
+
     def get(self, *args, **kwargs):
+        logger.info('Information incoming!')
         return Response(
             {
                 "status_code": 200,
