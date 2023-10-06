caa26ecd2ec571aa5b571a5c3025a3a0895ea614
VitaliiKosyk <kostamtik@gmail.com>
2023-10-06 22:50:37 +0300

    add linter runner
    add cache

--- apps/health_check/views.py
+++ apps/health_check/views.py
@@ -1,5 +1,6 @@
 import logging
 
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -8,6 +9,8 @@ logger = logging.getLogger(__name__)
 
 class HealthCheck(APIView):
     """A view for performing health checks."""
+
+    permission_classes = (AllowAny,)
 
     def get(self, *args, **kwargs):
         """Handle GET request for health check."""
