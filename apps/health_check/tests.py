3548f0ed9272cfbeface5f120fa11da689be81d1
VitaliiKosyk <kostamtik@gmail.com>
2023-10-09 11:30:33 +0300

    add files to exclude for linter,

--- apps/health_check/tests.py
+++ apps/health_check/tests.py
@@ -2,7 +2,10 @@ from rest_framework.test import APITestCase
 
 
 class HealthCheckTestCase(APITestCase):
+    """Test case for the health check endpoint."""
+
     def test_health_check_endpoint(self):
+        """Test the health check endpoint."""
         response = self.client.get('/health_check')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, {"status_code": 200, "detail": "ok", "result": "working"})
