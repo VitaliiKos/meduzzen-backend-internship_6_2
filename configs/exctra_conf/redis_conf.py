fff053f7afa8988018ccabc1946d1b48a338905c
VitaliiKosyk <kostamtik@gmail.com>
2023-11-09 13:15:49 +0200

    add Pydentic

--- configs/exctra_conf/redis_conf.py
+++ configs/exctra_conf/redis_conf.py
@@ -3,7 +3,7 @@ import os
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.environ.get('REDIS_URL'),
+        "LOCATION": "redis://redis:6379/0",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
