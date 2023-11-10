dc85e2124d00f4df3df860db536cdff24db49682
VitaliiKosyk <kostamtik@gmail.com>
2023-11-10 19:35:57 +0200

    export data: - Users can export quiz result data by quiz ID. - Owner or company's admin can export quiz result data for the current member of the company. - Owner or company's admin can export quiz result data for all members by the current quiz. - Add a temporary folder for exporting data files.

--- apps/quizzes/quiz_workflow/views.py
+++ apps/quizzes/quiz_workflow/views.py
@@ -1,7 +1,15 @@
-from rest_framework.generics import ListCreateAPIView, get_object_or_404
+from rest_framework.exceptions import APIException
+from rest_framework.generics import ListAPIView, ListCreateAPIView, get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 
 from apps.quizzes.models import QuizModel
+from apps.quizzes.quiz_workflow.helper import (
+    create_export_data,
+    create_export_response,
+    create_file_name,
+    filter_by_company,
+    filter_by_quiz,
+)
 from apps.quizzes.quiz_workflow.models import QuizResultModel
 from apps.quizzes.quiz_workflow.serializers import QuizResultSerializer
 
@@ -15,3 +23,32 @@ class QuizWorkflow(ListCreateAPIView):
         quiz_id = self.request.query_params.get('quiz_id')
         quiz = get_object_or_404(QuizModel, pk=quiz_id)
         serializer.save(user=self.request.user, company=quiz.company, quiz=quiz)
+
+
+class QuizResultCSVExportView(ListAPIView):
+    serializer_class = QuizResultSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer(self, queryset, many=True):
+        return self.serializer_class(queryset, many=many, )
+
+    def get_queryset(self):
+        params_dict = self.request.query_params.dict()
+        if 'company_id' in params_dict:
+            return filter_by_company(params_dict, authenticated_user=self.request.user)
+
+        elif 'quiz_id' in params_dict:
+            return filter_by_quiz(params_dict, authenticated_user=self.request.user)
+
+        raise APIException({'detail': 'Bad request'})
+
+    def list(self, request, *args, **kwargs):
+        params_dict = self.request.query_params.dict()
+        export_format = params_dict.get('file_format', None)
+
+        filename = create_file_name(params_dict=params_dict, user=self.request.user)
+
+        export_data = create_export_data(quiz_votes_list=self.get_queryset(), serializer=QuizResultSerializer,
+                                         file_format=export_format)
+        response = create_export_response(filename=filename, content=export_data, file_format=export_format)
+        return response
